39d5dd0c115c478c6b5fb27ec13bc613bb43f72b
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2022-07-29 11:46:38 -0500

    the program doest stop in TypeError

--- main.py
+++ main.py
@@ -10,20 +10,59 @@ if __name__ == '__main__':
             self.field = field
             self.fieldcount = fieldcount
 
-    excel1 = pd.read_excel('C:\\Users\\kevin\\Downloads\\excel_entreg\\libro2.1.xlsx', sheet_name='Hoja2')
+
+    excel1 = pd.read_excel('C:\\Users\\1001020411\\Downloads\\libro22.xlsx', sheet_name='Hoja2')
 
     newdt = pd.DataFrame(columns=['id', 'phone'])
 
     for i in tqdm(excel1.iterrows(), total=excel1.shape[0]):
-        #write a nue column in the new excel for each row
-        newdt.loc[len(newdt)] = [i[1][1], i[1][3]]
-        newdt.loc[len(newdt)] = [i[1][1], i[1][4]]
-        newdt.loc[len(newdt)] = [i[1][1], i[1][5]]
-        newdt.loc[len(newdt)] = [i[1][1], i[1][6]]
-        newdt.loc[len(newdt)] = [i[1][1], i[1][7]]
-        newdt.loc[len(newdt)] = [i[1][1], i[1][8]]
-        newdt.loc[len(newdt)] = [i[1][1], i[1][9]]
-        newdt.loc[len(newdt)] = [i[1][1], i[1][10]]
+        # write a nue column in the new excel for each row
+        try:
+            if ~np.isnan(i[1][3]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][3]]
+        except TypeError:
+            print(i[1][3])
+
+        try:
+            if ~np.isnan(i[1][4]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][4]]
+        except TypeError:
+            print(i[1][4])
+
+        try:
+            if ~np.isnan(i[1][5]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][5]]
+        except TypeError:
+            print(i[1][5])
+        try:
+            if ~np.isnan(i[1][6]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][6]]
+        except TypeError:
+            print(i[1][6])
+
+        try:
+            if ~np.isnan(i[1][7]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][7]]
+        except TypeError:
+            print(i[1][7])
+
+        try:
+            if ~np.isnan(i[1][8]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][8]]
+        except TypeError:
+            print(i[1][8])
+
+        try:
+            if ~np.isnan(i[1][9]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][9]]
+        except TypeError:
+            print(i[1][9])
+
+        try:
+            if ~np.isnan(i[1][10]):
+                newdt.loc[len(newdt)] = [i[1][1], i[1][10]]
+        except TypeError:
+            print(i[1][10])
 
         # save the new dataframe to a csv file
     try:
@@ -32,12 +71,12 @@ if __name__ == '__main__':
 
         for chunck, subset in enumerate(tqdm(chunks)):
             if chunck == 0:  # first row
-                newdt.loc[subset].to_csv('data.csv', mode='w', index=False, encoding="latin1")
+                newdt.loc[subset].to_csv('C:\\Users\\1001020411\\Downloads\\new.csv', mode='w', index=False,
+                                         encoding="latin1")
             else:
-                newdt.loc[subset].to_csv('data.csv', header=None, mode='a', index=False, encoding="latin1")
+                newdt.loc[subset].to_csv('C:\\Users\\1001020411\\Downloads\\new.csv', header=None, mode='a',
+                                         index=False, encoding="latin1")
 
         print('Done!')
     except PermissionError:
         print('Error: The file is open, please close it and try again')
-
-
